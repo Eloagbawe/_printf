f45deeca2e314fc938cd8b5a80618c0e570a85c0
Eloagbawe <eloagbawe@gmail.com>
2023-10-26 18:41:04 +0100

    update print and handle specifier functions

--- _print.c
+++ _print.c
@@ -8,7 +8,7 @@
  * Return: void
 */
 void _putchar(int c) {
-    write(1, &c, 1);
+  write(1, &c, 1);
 }
 
 /**
@@ -17,12 +17,12 @@ void _putchar(int c) {
  * Return: length of the string
 */
 int _strlen(const char *str) {
-    int i = 0;
+  int i = 0;
 
-    while (str[i] != '\0') {
-        i++;
-    }
-    return i;
+  while (str[i] != '\0') {
+    i++;
+  }
+  return i;
 }
 
 /**
@@ -31,13 +31,29 @@ int _strlen(const char *str) {
  * Return: number of chars printed
 */
 
-int _printstr(const char * s) {
-    int i;
+int _printstr(const char *s) {
+  int i, len;
+  char buffer[1024];
+  size_t buffer_size = sizeof(buffer);
+  size_t dataSize = 0;
+  
+  len = _strlen(s);
 
-    for (i = 0; i < _strlen(s); i++) {
-        _putchar(s[i]);
+  for (i = 0; i < len; i++) {
+
+    if (dataSize < buffer_size) {
+      buffer[dataSize++] = s[i];
+    } else {
+      write(1, buffer, dataSize);
+      dataSize = 0;
+      buffer[dataSize++] = s[i];
     }
-    return _strlen(s);
+  }
+
+  if (dataSize > 0) {
+    write(1, buffer, dataSize);
+  }
+  return len;
 }
 
 /**
@@ -47,20 +63,20 @@ int _printstr(const char * s) {
 */
 
 int _printnum(long int num, int* i) {
-    long int x;
+  long int x;
 
-    if (num < 0) {
-        num *= -1;
-        _putchar('-');
-        *i += 1;
-    }
-    x = num;
-    if (num > 0) {
-        *i += 1;
-        _printnum(num / 10, i);
-        _putchar(48 + (x % 10));
-    }
-    return *i;
+  if (num < 0) {
+    num *= -1;
+    _putchar('-');
+    *i += 1;
+  }
+  x = num;
+  if (num > 0) {
+    *i += 1;
+    _printnum(num / 10, i);
+    _putchar(48 + (x % 10));
+  }
+  return *i;
 }
 
 
@@ -70,5 +86,5 @@ int _printnum(long int num, int* i) {
 */
 
 int _printnull(void) {
-    return _printstr("(null)");
+  return _printstr("(null)");
 }
