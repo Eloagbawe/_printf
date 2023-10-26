fae8d92c8ed879f8cdc103cc7d992864b52020f9
Eloagbawe <eloagbawe@gmail.com>
2023-10-27 00:23:14 +0100

    add function to handle %p specifier

---
+++ _handle_specifiers_4.c
@@ -0,0 +1,25 @@
+#include "_printf.h"
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+ * _handle_p - handles the p specifier
+ * @add: address to print
+ * Return: num of characters printed
+*/
+
+int _handle_p(unsigned long int add) {
+  int printed = 0;
+  // unsigned long int add = va_arg(args, unsigned long int);
+  uintptr_t address_as_uint = (uintptr_t)add;
+
+  printed += _printstr("0x");
+
+  if (address_as_uint == 0) {
+    return printed + _printzero();
+  } else {
+    int count = 0, *count_ptr = NULL;
+    count_ptr = &count;
+    return printed + _printhexa(address_as_uint, count_ptr);
+  }
+}
